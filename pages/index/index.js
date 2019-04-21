10d873ed5f028bb5e4ba6a503b18f3c1c8d71961
Dominic Wong <dominic.wong.617@gmail.com>
2019-04-21 21:18:42 +0800

    News list page

---
+++ pages/index/index.js
@@ -0,0 +1,50 @@
+Page({
+  data: {
+    categories: [
+      { name: "国内", type: "gn" },
+      { name: "国际", type: "gj" },
+      { name: "财经", type: "cj" },
+      { name: "娱乐", type: "yl" },
+      { name: "军事", type: "js" },
+      { name: "体育", type: "ty" },
+      { name: "其他", type: "other" }
+    ],
+    activeCategory: "gn",
+    hottestNews: null,
+    news: []   
+  },
+
+  onLoad() {
+    this.getNews();
+  },
+
+  getNews() {
+    wx.request({
+      url: `https://test-miniprogram.com/api/news/list?type=${this.data.activeCategory}`,
+      success: res => {
+        let news = res.data.result;
+        news.forEach((newsItem) => {
+          newsItem.displayTime = this.timeDisplay(newsItem.date);
+        });
+        const hottestNews = news.splice(0, 1)[0];
+        this.setData({ hottestNews, news });
+      }
+    })
+  },
+
+  timeDisplay(datetime) {
+    const date = new Date(datetime);
+    const hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  },
+
+  selectCategory(event) {
+    const selectedCategory = event.target.dataset.category;
+    this.setData({ activeCategory: selectedCategory });
+    this.getNews();
+  }
+})
