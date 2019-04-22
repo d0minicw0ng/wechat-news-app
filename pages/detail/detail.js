5db0ba4410cf9a905f78de7e53590271505fe180
Dominic Wong <dominic.wong.617@gmail.com>
2019-04-22 11:13:28 +0800

    Detail page onPullDownRefresh

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -9,13 +9,21 @@ Page({
     this.setData({ id }, this.getNews);
   },
 
-  getNews() {
+  onPullDownRefresh() {
+    this.getNews(() => {
+      wx.stopPullDownRefresh();
+    })
+  },
+
+  getNews(callback) {
     wx.request({
       url: `https://test-miniprogram.com/api/news/detail?id=${this.data.id}`,
       success: res => {
         let news = res.data.result;
         news.displayTime = this.timeDisplay(news.date);
         this.setData({ news });
+
+        callback && callback();
       }
     })
   },
